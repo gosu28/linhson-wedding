805b652b4b1b44ea52e7e02e568c5b8bdef196b0
gosu28 <son.bjerg@gmail.com>
2023-12-02 14:53:12 +0700

    upload code

--- js/main.js
+++ js/main.js
@@ -18,8 +18,34 @@ $(window).on("load", function(){
 
 $(document).ready(function(){
 
+    // nav toggle
+    $(".hamburger-btn").click(function(){
+        $(".header .nav").slideToggle();
+    })
+
+    $(".header .nav").click(function(){
+        if ($(window).width() < 768) {
+            $(".header .nav").slideToggle();
+        }
+    })
+
+    // fixed header
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $(".header").addClass("fixed");
+        }
+        else {
+            $(".header").removeClass("fixed");
+        }
+    })
+
+    // scroll
+    $.scrollIt({
+        topOffset: -50
+    });
+
+    // people filter
     peopleFilter($(".filter-btn.active").attr("data-target"))
-    
     $(".filter-btn").click(function(){
         if (!$(this).hasClass("active")) {
             peopleFilter($(this).attr("data-target"));
